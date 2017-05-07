7a7e236e2884335de080a394781578c73644556e
Anika <salixbabyloniica@gmail.com>
2017-05-07 23:55:07 +0600

    added fixtures and testcase

--- chats/tests.py
+++ chats/tests.py
@@ -1,15 +1,17 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 
 class ChatsViewsTestCase(TestCase):
     """
-    This test case checks if a logged can access inbox and if inbox view context
+    This test case checks if a logged in user can access inbox and if inbox view context
     contains these keys: user list, media root url and logged in user information
     """
     fixtures = ['chats_data.json']
 
     def setUp(self):
-        self.client.login(username='Lia', password='123')
+        user = User.objects.create_user(username='sara', password='123')
+        self.client.login(username='sara', password='123')
 
     def test_inbox(self):
         resp = self.client.get('/chats/inbox/')
